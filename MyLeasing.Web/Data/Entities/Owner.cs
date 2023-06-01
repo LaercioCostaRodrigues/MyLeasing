00e9f3bfb52e30f380d4ad76f43e42606828eb14
Laercio Costa Rodrigues <laercio.cr.it@gmail.com>
2023-06-01 23:16:09 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(9)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(9)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        [Display(Name = "Owner Name")]
+        public string OwnerName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+    }
+}
---
+++ MyLeasing.Web/Migrations/20230601205303_InitDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLeasing.Web.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Document = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    FixedPhone = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    CellPhone = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
