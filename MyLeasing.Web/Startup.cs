067fe2274768b3d900984a33c079c9314d2a1f98
Laercio Costa Rodrigues <laercio.cr.it@gmail.com>
2023-06-03 20:00:28 +0100

    Repository Pattern

--- MyLeasing.Web/Startup.cs
+++ MyLeasing.Web/Startup.cs
@@ -32,6 +32,8 @@ namespace MyLeasing.Web
 
             services.AddTransient<SeedDb>();
 
+            services.AddScoped<IRepository, Repository>();
+
             services.AddControllersWithViews();
         }
 
